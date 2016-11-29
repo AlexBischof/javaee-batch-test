86e9ee934802865d323dcd97a4f88a616be36103
AlexBischof <alexanderxbischof@gmail.com>
2016-11-29 18:38:03 +0100

    Add JobContext injection

--- src/main/java/de/bischinger/MyBatchletCdi.java
+++ src/main/java/de/bischinger/MyBatchletCdi.java
@@ -1,10 +1,12 @@
 package de.bischinger;
 
 import javax.batch.api.AbstractBatchlet;
+import javax.batch.runtime.context.JobContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 
+import static java.lang.String.format;
 import static javax.batch.runtime.BatchStatus.COMPLETED;
 
 /**
@@ -16,9 +18,12 @@ public class MyBatchletCdi extends AbstractBatchlet {
     @Inject
     private EntityManager em;
 
+    @Inject
+    JobContext jobContext;
+
     @Override
     public String process() throws Exception {
-        System.out.println("MyBatchletCdi: " + em);
+        System.out.println(format("MyBatchletCdi: %s %s", em, jobContext));
 
         return COMPLETED.toString();
     }
--- src/main/java/de/bischinger/MyBatchletPersistenceContext.java
+++ src/main/java/de/bischinger/MyBatchletPersistenceContext.java
@@ -1,22 +1,30 @@
 package de.bischinger;
 
 import javax.batch.api.AbstractBatchlet;
+import javax.batch.runtime.context.JobContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import static java.lang.String.format;
 import static javax.batch.runtime.BatchStatus.COMPLETED;
 
 /**
  * Created by bischofa on 29/11/16.
  */
+@Named
 public class MyBatchletPersistenceContext extends AbstractBatchlet {
 
     @PersistenceContext
     private EntityManager em;
 
+    @Inject
+    JobContext jobContext;
+
     @Override
     public String process() throws Exception {
-        System.out.println("MyBatchletPersistenceContext: " + em);
+        System.out.println(format("MyBatchletPersistenceContext: %s %s", em, jobContext));
 
         return COMPLETED.toString();
     }
